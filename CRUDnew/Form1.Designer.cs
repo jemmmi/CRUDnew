d9695359796b1d2b759c70a75ec7fec814932653
Jemsheer <Jemsheer@DESKTOP-163LJK7>
2023-08-02 12:11:51 +0530

    Add project files.

---
+++ CRUDnew/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace CRUDnew
+{
+    partial class FormEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_city = new System.Windows.Forms.TextBox();
+            this.cb_gender = new System.Windows.Forms.ComboBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_clr = new System.Windows.Forms.Button();
+            this.btn_dlt = new System.Windows.Forms.Button();
+            this.datagv = new System.Windows.Forms.DataGridView();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_dept = new System.Windows.Forms.TextBox();
+            this.tb_ph = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.datagv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(191, 16);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(252, 20);
+            this.tb_name.TabIndex = 4;
+            this.tb_name.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.tb_name_PreviewKeyDown);
+            // 
+            // tb_city
+            // 
+            this.tb_city.Location = new System.Drawing.Point(191, 51);
+            this.tb_city.Name = "tb_city";
+            this.tb_city.Size = new System.Drawing.Size(252, 20);
+            this.tb_city.TabIndex = 5;
+            this.tb_city.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.tb_city_PreviewKeyDown);
+            // 
+            // cb_gender
+            // 
+            this.cb_gender.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cb_gender.FormattingEnabled = true;
+            this.cb_gender.Items.AddRange(new object[] {
+            "male",
+            "female"});
+            this.cb_gender.Location = new System.Drawing.Point(191, 115);
+            this.cb_gender.Name = "cb_gender";
+            this.cb_gender.Size = new System.Drawing.Size(252, 21);
+            this.cb_gender.TabIndex = 7;
+            this.cb_gender.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.cb_gender_PreviewKeyDown);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(55, 190);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 8;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_clr
+            // 
+            this.btn_clr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clr.Location = new System.Drawing.Point(173, 190);
+            this.btn_clr.Name = "btn_clr";
+            this.btn_clr.Size = new System.Drawing.Size(75, 23);
+            this.btn_clr.TabIndex = 9;
+            this.btn_clr.Text = "Clear";
+            this.btn_clr.UseVisualStyleBackColor = true;
+            this.btn_clr.Click += new System.EventHandler(this.btn_clr_Click);
+            // 
+            // btn_dlt
+            // 
+            this.btn_dlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dlt.Location = new System.Drawing.Point(301, 190);
+            this.btn_dlt.Name = "btn_dlt";
+            this.btn_dlt.Size = new System.Drawing.Size(75, 23);
+            this.btn_dlt.TabIndex = 10;
+            this.btn_dlt.Text = "Delete";
+            this.btn_dlt.UseVisualStyleBackColor = true;
+            this.btn_dlt.Click += new System.EventHandler(this.btn_dlt_Click);
+            // 
+            // datagv
+            // 
+            this.datagv.AllowUserToAddRows = false;
+            this.datagv.AllowUserToOrderColumns = true;
+            this.datagv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagv.Location = new System.Drawing.Point(14, 238);
+            this.datagv.Name = "datagv";
+            this.datagv.RowHeadersVisible = false;
+            this.datagv.Size = new System.Drawing.Size(513, 152);
+            this.datagv.TabIndex = 11;
+            this.datagv.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagv_CellDoubleClick);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_show.Location = new System.Drawing.Point(420, 190);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(75, 23);
+            this.btn_show.TabIndex = 12;
+            this.btn_show.Text = "Show";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(61, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Phone no:";
+            // 
+            // tb_dept
+            // 
+            this.tb_dept.Location = new System.Drawing.Point(191, 84);
+            this.tb_dept.Name = "tb_dept";
+            this.tb_dept.Size = new System.Drawing.Size(252, 20);
+            this.tb_dept.TabIndex = 6;
+            this.tb_dept.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.tb_dept_PreviewKeyDown);
+            // 
+            // tb_ph
+            // 
+            this.tb_ph.Location = new System.Drawing.Point(191, 148);
+            this.tb_ph.Name = "tb_ph";
+            this.tb_ph.Size = new System.Drawing.Size(252, 20);
+            this.tb_ph.TabIndex = 14;
+            this.tb_ph.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.tb_ph_PreviewKeyDown);
+            // 
+            // FormEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.tb_ph);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.datagv);
+            this.Controls.Add(this.btn_dlt);
+            this.Controls.Add(this.btn_clr);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.cb_gender);
+            this.Controls.Add(this.tb_dept);
+            this.Controls.Add(this.tb_city);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEmployee";
+            this.Text = "FormEmployee";
+            this.Load += new System.EventHandler(this.FormEmployee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datagv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_city;
+        private System.Windows.Forms.ComboBox cb_gender;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_clr;
+        private System.Windows.Forms.Button btn_dlt;
+        private System.Windows.Forms.DataGridView datagv;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_dept;
+        private System.Windows.Forms.TextBox tb_ph;
+    }
+}
+
---
+++ CRUDnew/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDnew
+{
+    public partial class FormEmployee : Form
+    {
+        SqlConnection sqlCon;
+        SqlCommand sqlCmd;
+        string Staff_id = "";
+        string conString = "Data Source=DESKTOP-163LJK7\\MICTCO;Initial Catalog=CRUD;Integrated Security=True";
+        public FormEmployee()
+        {
+            InitializeComponent();
+            sqlCon = new SqlConnection(conString);
+            sqlCon.Open();
+        }
+
+        private void FormEmployee_Load(object sender, EventArgs e)
+        {
+            datagv.AutoGenerateColumns = false;
+            datagv.DataSource = FetchStaffDetails();
+            FetchStaffDetails();
+        }
+        private DataTable FetchStaffDetails()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable dtData = new DataTable();
+            sqlCmd = new SqlCommand("spStaff", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchData");
+            SqlDataAdapter sqlSda = new SqlDataAdapter(sqlCmd);
+            sqlSda.Fill(dtData);
+            //if(dtData.Rows.Count>0)
+            //{
+            //    for(int i=0; i <= dtData.Rows.Count - 1; i++) 
+            //    {
+            //        if (dtData.Rows.Count >= datagv.Rows.Count)
+            //        {
+            //            datagv.Rows.Add();
+            //        }
+
+            //        datagv.Rows[i].Cells["StaffId"].Value = dtData.Rows[i]["Staff_id"].ToString();
+            //        datagv.Rows[i].Cells["name"].Value = dtData.Rows[i]["Name"].ToString();
+            //        datagv.Rows[i].Cells["city"].Value = dtData.Rows[i]["City"].ToString();
+            //        datagv.Rows[i].Cells["department"].Value = dtData.Rows[i]["Department"].ToString();
+            //        datagv.Rows[i].Cells["gender"].Value = dtData.Rows[i]["Gender"].ToString();
+            //    }
+            //}
+            return dtData;
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tb_name.Text))
+            {
+                MessageBox.Show("Enter Employee Name");
+                tb_name.Select();
+            }
+            else if (string.IsNullOrWhiteSpace(tb_city.Text))
+            {
+                MessageBox.Show("Enter Current City");
+                tb_city.Select();
+            }
+            else if (string.IsNullOrWhiteSpace(tb_dept.Text))
+            {
+                MessageBox.Show("Enter Department");
+                tb_dept.Select();
+            }
+            else if (cb_gender.SelectedIndex <= -1)
+            {
+                MessageBox.Show("Select Gender");
+                cb_gender.Select();
+            }
+            else if (string.IsNullOrWhiteSpace(tb_ph.Text))
+            {
+                MessageBox.Show("Enter PhoneNo");
+            }
+            else
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    string actionType = "";
+                    actionType = btn_save.Text == "Save" ? "SaveData" : "UpdateData";
+                    DataTable dtData = new DataTable();
+                    sqlCmd = new SqlCommand("spStaff", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", actionType);
+                    sqlCmd.Parameters.AddWithValue("@Staff_id", Staff_id);
+                    sqlCmd.Parameters.AddWithValue("@Name", tb_name.Text);
+                    sqlCmd.Parameters.AddWithValue("@City", tb_city.Text);
+                    sqlCmd.Parameters.AddWithValue("@Department", tb_dept.Text);
+                    sqlCmd.Parameters.AddWithValue("@Gender", cb_gender.Text);
+                    sqlCmd.Parameters.AddWithValue("@PhoneNo", tb_ph.Text);
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Successfully Entered");
+                        ClearAllData();
+                    }
+                    else
+                        MessageBox.Show("Enter Again");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("error:-" + ex.Message);
+                }
+            }
+        }
+
+        private void datagv_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                btn_save.Text = "Update";
+                Staff_id = datagv.Rows[e.RowIndex].Cells[0].Value.ToString();
+                DataTable dtData = FetchStaffRecords(Staff_id);
+                if (dtData.Rows.Count > 0)
+                {
+                    Staff_id = dtData.Rows[0][0].ToString();
+                    tb_name.Text = dtData.Rows[0][1].ToString();
+                    tb_city.Text = dtData.Rows[0][2].ToString();
+                    tb_dept.Text = dtData.Rows[0][3].ToString();
+                    cb_gender.Text = dtData.Rows[0][4].ToString().Trim();
+                    tb_ph.Text = dtData.Rows[0][5].ToString();
+                }
+                else
+                {
+                    ClearAllData();
+                }
+            }
+        }
+        private DataTable FetchStaffRecords(string Staff_id)
+        {
+
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            DataTable dtData = new DataTable();
+            sqlCmd = new SqlCommand("spStaff", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ActionType", "FetchRecord");
+            sqlCmd.Parameters.AddWithValue("@Staff_id", Staff_id);
+            SqlDataAdapter sqlSda = new SqlDataAdapter(sqlCmd);
+            sqlSda.Fill(dtData);
+            return dtData;
+        }
+
+        private void btn_dlt_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Staff_id))
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    DataTable dtData = new DataTable();
+                    sqlCmd = new SqlCommand("spStaff", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", "DeleteData");
+                    sqlCmd.Parameters.AddWithValue("@Staff_id", Staff_id);
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Record Deleted Successfully");
+                        ClearAllData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pls try again");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("error:-" + ex.Message);
+                }
+            else
+            {
+                MessageBox.Show("Please Select A Record !!!");
+            }
+        }
+
+        private void btn_clr_Click(object sender, EventArgs e)
+        {
+            ClearAllData();
+        }
+        public void ClearAllData()
+        {
+            btn_save.Text = "Save";
+            tb_name.Text = "";
+            tb_city.Text = "";
+            tb_dept.Text = "";
+            cb_gender.SelectedIndex = -1;
+            tb_ph.Text = "";
+            Staff_id = "";
+            FetchStaffDetails();
+            datagv.DataSource = null;
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlCommand cmd = new SqlCommand("display", sqlCon);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dtData = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dtData);
+            datagv.AutoGenerateColumns = true;
+            //dtData.Load(cmd.ExecuteReader());
+            datagv.DataSource = dtData;
+
+            sqlCon.Close();
+            //if (dtData.Rows.Count > 0)
+            //{
+            //    for (int i = 0; i <= dtData.Rows.Count - 1; i++)
+            //    {
+            //        if (dtData.Rows.Count >= datagv.Rows.Count)
+            //        {
+            //            datagv.Rows[i].Cells["StaffId"].Value = dtData.Rows[i]["Staff_id"].ToString();
+            //            datagv.Rows[i].Cells["name"].Value = dtData.Rows[i]["Name"].ToString();
+            //            datagv.Rows[i].Cells["city"].Value = dtData.Rows[i]["City"].ToString();
+            //            datagv.Rows[i].Cells["department"].Value = dtData.Rows[i]["Department"].ToString();
+            //            datagv.Rows[i].Cells["gender"].Value = dtData.Rows[i]["Gender"].ToString();
+            //            datagv.Rows[i].Cells["phone"].Value = dtData.Rows[i]["PhoneNo"].ToString();
+            //        }
+            //    }
+            //}
+
+        }
+
+        private void tb_name_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlConnection connection = new SqlConnection(conString);
+                SqlDataAdapter adap = new SqlDataAdapter("select * from tblStaff", sqlCon);
+                DataTable dtData = new DataTable();
+                adap.Fill(dtData);
+
+                for (int i = 0; i < dtData.Rows.Count; i++)
+                {
+                    if (tb_name.Text == dtData.Rows[i]["Name"].ToString())
+                    {
+                        MessageBox.Show("duplicate value");
+                        return;
+                    }
+                    else
+                    {
+                        tb_city.Focus();
+                    }
+                }
+            }
+        }
+        private void tb_city_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                tb_dept.Focus();
+            }
+        }
+
+        private void tb_dept_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                cb_gender.Focus();
+            }
+        }
+
+        private void cb_gender_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                tb_ph.Focus();
+            }
+        }
+
+        private void tb_ph_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                SqlConnection connection = new SqlConnection(conString);
+                SqlDataAdapter adap = new SqlDataAdapter("select * from tblStaff", sqlCon);
+                DataTable dtData = new DataTable();
+                adap.Fill(dtData);
+                //Regex phoneNumpattern = new Regex(@"\+[0-9]{3}\s+[0-9]{3}\s+[0-9]{5}\s+[0-9]{3}");
+                //if (phoneNumpattern.IsMatch(tb_ph.Text))
+                //{
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Invalid Format");
+                //}
+                for (int i = 0; i < dtData.Rows.Count; i++)
+                {
+                    if (tb_ph.Text == dtData.Rows[i]["PhoneNo"].ToString())
+                    {
+                        MessageBox.Show("Number already EXIST");
+                        return;
+                    }
+                    else
+                    {
+                        btn_save.Focus();
+                    }
+                }
+            }
+        }
+    }
+}
